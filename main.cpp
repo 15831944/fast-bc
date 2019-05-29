0fa60bdbfee63db8ed933b106fe2e51afb2137b2
Davide Croci <daddemc@gmail.com>
2019-05-29 14:28:52 +0200

    Removed shit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graph_utility.hpp>
-#include <boost/graph/betweenness_centrality.hpp>
-#include <boost/property_map/shared_array_property_map.hpp>
 #include <map>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <utility>
-
-#include <fastbc.h>
-#include <louvain.h>
-
-// TODO choose the right data structures 
-// https://www.boost.org/doc/libs/1_55_0/libs/graph/doc/using_adjacency_list.html
-
-using namespace boost;
 
 char *filename 		= NULL;
 char *filename_o 	= NULL;
@@ -67,63 +55,6 @@ int main(int argc, char **argv)
 
 	//**************************************************************************************
 	// READ GRAPH
-
-	Graph g;
-    auto ensure_node = [&g, known=std::map<int, Graph::vertex_descriptor>{}](int id) mutable {
-        if (auto it = known.find(id); it == known.end())
-            return known.emplace(id, add_vertex(id, g)).first->second;
-        else
-            return it->second;
-    };
-
-    std::ifstream ifs(filename);
-
-    for (std::string s; getline(ifs, s);) {
-        int from,to;
-        double weight;
-        if (std::istringstream(s) >> from >> to >> weight) {
-            add_edge(ensure_node(from), ensure_node(to), weight, g);
-        }
-        else break;
-    }
-    ifs.close();
-
-    if (ifs.eof()) {
-        std::cout << "Graph parsed with " << num_edges(g) << " edges and " << num_vertices(g) << " vertices\n";
-    } else {
-        ifs.clear();
-        ifs.close();
-        std::cout << "Parse error\n";
-        exit(EXIT_FAILURE);
-    }
-
-    if (ifs) {
-        std::cout << "Remaining unparsed input: '" << ifs.rdbuf() << "'\n";
-        ifs.close();
-        exit(EXIT_FAILURE);
-    } else {
-   		ifs.close();
-    }
-
-    //**************************************************************************************
-	// PERFORM LOUVAIN
-
-	Louvain louvain(g);
-	std::vector<int> order = louvain.random_evaluation_order();
-	Partition p = louvain.compute(order);
-
-    //**************************************************************************************
-	// PERFORM BRANDES
-
-	VertexIndexMap v_index = get(vertex_index, g);
-	boost::shared_array_property_map<double, VertexIndexMap >
-		centrality_map(num_vertices(g), v_index);
-
-    brandes_betweenness_centrality(g, centrality_map);
-
-    /*BGL_FORALL_VERTICES_T(v, g, Graph) {
-    	std::cout << v_index[v] << " " << get(centrality_map, v) << std::endl;
-	}*/
 
     return 0;
 }
